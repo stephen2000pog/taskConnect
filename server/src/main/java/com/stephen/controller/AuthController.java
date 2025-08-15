ac97b12bd954ef8ff9cf8d359cd1041ac6b8bfa2
stephen <lee.stephen@courrier.uqam.ca>
2025-08-15 16:50:31 -0400

    addding controller

---
+++ server/src/main/java/com/stephen/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.stephen.controller;
+
+import com.stephen.dto.LoginDTO;
+import com.stephen.dto.RegisterDTO;
+import com.stephen.service.AuthService;
+import com.stephen.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDTO request) {
+        String token = authService.authenticate(request.getEmail(), request.getPassword());
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDTO request) {
+        String token = authService.register(request.getEmail(), request.getPassword(), request.getRole());
+        return ResponseEntity.ok(token);
+    }
+
+}
+
