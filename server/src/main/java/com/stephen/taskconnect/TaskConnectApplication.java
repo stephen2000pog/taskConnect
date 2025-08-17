73e236cf8eb7dd2d3be06a24d7d491d223542f48
stephen <lee.stephen@courrier.uqam.ca>
2025-08-17 14:44:17 -0400

    adding file

---
+++ server/src/main/java/com/stephen/taskconnect/TaskConnectApplication.java
@@ -0,0 +1,13 @@
+package com.stephen.taskconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskConnectApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/taskconnect/taskconnect/TaskConnectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.taskconnect.taskconnect;
+
+import com.stephen.taskconnect.TaskConnectApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TaskConnectApplication.class)
+public class TaskConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
